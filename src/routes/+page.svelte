591e9b1c44834dbb2692caaa585938b02f79d877
James Weaver <james.l.weaver@gmail.com>
2023-08-25 15:15:06 -0400

    Tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <h1>Life Planning Assistant</h1>
 <!-- Add two links, one for Goals https://lpa-ui.vercel.app/goals and one for Calendar https://lpa-ui.vercel.app/calendar-->
-<!-- <a href="https://lpa-ui.vercel.app/goals">Goals</a>
-<a href="https://lpa-ui.vercel.app/calendar">Calendar</a> -->
-<a href="http://localhost:5173/goals">Goals</a>
-<a href="http://localhost:5173/calendar">Calendar</a>
+<a href="https://lpa-ui.vercel.app/goals">Goals</a>
+<a href="https://lpa-ui.vercel.app/calendar">Calendar</a>
+<!-- <a href="http://localhost:5173/goals">Goals</a>
+<a href="http://localhost:5173/calendar">Calendar</a> -->
 
 
 
