244bf68054c5be9a094c682d7d321cb139f51bba
James Weaver <james.l.weaver@gmail.com>
2023-09-29 13:42:31 -0400

    Change priority to 1-9

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -79,19 +79,18 @@
   if (!SHOW_COMPLETED_TASKS) {
    tasks = tasks.filter(task => task.status.toLowerCase() != 'completed');
   }
-  // Sort the tasks by priority, where high_priority is first, then medium_priority, then low_priority
+  // Sort the tasks by numerical priority "1" through "9"
   tasks.sort((a, b) => {
-   if (a.priority.startsWith('high') && !b.priority.startsWith('high')) {
+   if (a.priority < b.priority) {
     return -1;
-   } else if (a.priority.startsWith('medium') && b.priority.startsWith('low')) {
-    return -1;
-   } else if (a.priority.startsWith('low') && b.priority.startsWith('medium')) {
+   } else if (a.priority > b.priority) {
     return 1;
    } else {
     return 0;
    }
   });
   console.log(tasks, 'tasks');
+  
   // Create a line for each task
   let tasksString = '';
   for (let i = 0; i < tasks.length; i++) {
@@ -99,7 +98,7 @@
    let b_or_p = tasks[i].for_business ? 'business' : 'personal';
    let recurrence_str = tasks[i].recurrence ? ', recurrence: ' + tasks[i].recurrence : '';
    let dtstart_str = tasks[i].dtstart ? ', dtstart: ' + tasks[i].dtstart : '';
-   tasksString += '\n• ' + tasks[i].task_name + ', ' + b_or_p  + ', ' + tasks[i].task_hrs_float + ' hrs, ' + tasks[i].priority + ', ' + tasks[i].status + recurrence_str + dtstart_str;
+   tasksString += '\n• ' + tasks[i].task_name + ', ' + b_or_p  + ', ' + tasks[i].task_hrs_float + ' hrs, priority: ' + tasks[i].priority + ', ' + tasks[i].status + recurrence_str + dtstart_str;
   }
   let goal_display_date = goal.timeframe_date;
   let goal_status_str = ", " + goal.status;
