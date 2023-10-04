2ccacd924c69e0b6425301b32050c5d11a8c75c9
James Weaver <james.l.weaver@gmail.com>
2023-10-04 06:53:38 +0200

    update URL

--- src/routes/calendar/+page.server.js
+++ src/routes/calendar/+page.server.js
@@ -3,7 +3,7 @@ export async function load({fetch, params}) {
  try {
   // https://life-planning-assistant-JavaFXpert.replit.app/fetch_calendar_summary
   const URL = `
-  https://life-planning-assistant.replit.app/fetch_calendar_summary`;
+  https://lpa.javafxpert.repl.co/fetch_calendar_summary`;
   const res = await fetch(URL);
   const lpaEvents = await res.json();
    return { lpaEvents };
--- src/routes/goals/+page.server.js
+++ src/routes/goals/+page.server.js
@@ -2,22 +2,22 @@
 export async function load({fetch, params}) {
  try {
   const MISSION_URL = `
-  https://life-planning-assistant.replit.app/fetch_personal_mission_statement`;
+  https://lpa.javafxpert.repl.co/fetch_personal_mission_statement`;
   const res_mission = await fetch(MISSION_URL);
   const mission = await res_mission.json();
 
   const VISION_URL = `
-  https://life-planning-assistant.replit.app/fetch_personal_vision_statement`;
+  https://lpa.javafxpert.repl.co/fetch_personal_vision_statement`;
   const res_vision = await fetch(VISION_URL);
   const vision = await res_vision.json();
 
   const SWOT_URL = `
-  https://life-planning-assistant.replit.app/fetch_personal_swot_analysis`;
+  https://lpa.javafxpert.repl.co/fetch_personal_swot_analysis`;
   const res_swot = await fetch(SWOT_URL);
   const swot = await res_swot.json();
 
   const GOALS_URL = `
-  https://life-planning-assistant.replit.app/fetch_goals_summary`;
+  https://lpa.javafxpert.repl.co/fetch_goals_summary`;
   const res_goals = await fetch(GOALS_URL);
   const goals = await res_goals.json();
 
